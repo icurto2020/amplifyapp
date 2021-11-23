56c156d814a30b5e87f9ad0929ac254abf37d9c9
Ines Curto <67243821+icurto2020@users.noreply.github.com>
2021-11-22 20:20:00 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
 
 /*
-CODE FROM HERE WORKED BELOW with some modification aws-exports
+CODE withAuthenticator FROM HERE WORKED BELOW with some modification aws-exports
 https://ui.docs.amplify.aws/components/authenticator#signupattributes
 */
 
 import { Amplify } from 'aws-amplify';
-
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
+// module 4 imports
+// Module 4 code
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API } from 'aws-amplify';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+// module 5 import
+import {Storage } from 'aws-amplify';
+//auth import
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
+const initialFormState = { name: '', description: '' }
+
+/* worked fine before for authenticator
 function App({ signOut, user }) {
   return (
     <>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
+  );
+}
+*/
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+//  async function fetchNotes() {
+//    const apiData = await API.graphql({ query: listNotes });
+//    setNotes(apiData.data.listNotes.items);
+//  }
+async function fetchNotes() {
+  const apiData = await API.graphql({ query: listNotes });
+  const notesFromAPI = apiData.data.listNotes.items;
+  await Promise.all(notesFromAPI.map(async note => {
+    if (note.image) {
+      const image = await Storage.get(note.image);
+      note.image = image;
+    }
+    return note;
+  }))
+  setNotes(apiData.data.listNotes.items);
+}
+
+//  async function createNote() {
+//    if (!formData.name || !formData.description) return;
+//    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+//    setNotes([ ...notes, formData ]);
+//    setFormData(initialFormState);
+//  }
+
+async function createNote() {
+  if (!formData.name || !formData.description) return;
+  await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  if (formData.image) {
+    const image = await Storage.get(formData.image);
+    formData.image = image;
+  }
+  setNotes([ ...notes, formData ]);
+  setFormData(initialFormState);
+}
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+// module 5
+async function onChange(e) {
+  if (!e.target.files[0]) return
+  const file = e.target.files[0];
+  setFormData({ ...formData, image: file.name });
+  await Storage.put(file.name, file);
+  fetchNotes();
+}
+//
+  return (
+    <div className="App">
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+	  <input
+        type="file"
+        onChange={onChange}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+       {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+              {
+               note.image && <img src={note.image} style={{width: 400}} />
+              }
+      </div>
+      ))
+     }
+      </div>
+      <signOut />
+    </div>
   );
 }
 
